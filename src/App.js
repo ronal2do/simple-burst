4328b2ffd0b40f0024fa17b3ef11b8a9def2082f
Ronaldo <ronaldo@SOTMAC16.local>
2017-01-03 11:02:56 -0200

    styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import mojs from 'mo-js';
 
+import './App.css';
+
 class Heart extends mojs.CustomShape {
   getShape () {
     return '<path d="M73.6170213,0 C64.4680851,0 56.5957447,5.53191489 51.7021277,13.8297872 C50.8510638,15.3191489 48.9361702,15.3191489 48.0851064,13.8297872 C43.4042553,5.53191489 35.3191489,0 26.1702128,0 C11.9148936,0 0,14.0425532 0,31.2765957 C0,48.0851064 14.893617,77.8723404 47.6595745,99.3617021 C49.1489362,100.212766 50.8510638,100.212766 52.1276596,99.3617021 C83.8297872,78.5106383 99.787234,48.2978723 99.787234,31.2765957 C100,14.0425532 88.0851064,0 73.6170213,0 L73.6170213,0 Z"></path>';
@@ -57,7 +59,7 @@ class App extends Component {
         const coords = { x: e.pageX, y: e.pageY };
 
          burst
-             .tune({ x: e.pageX, y: e.pageY })
+             .tune( coords )
              .replay();
          circle
             .tune( coords )
@@ -66,13 +68,16 @@ class App extends Component {
          heart
             .tune( coords )
             .replay();
+
       });
     }
     render() {
-      return <div>
-
-      </div>;
+      return (
+        <div className="Click-Zone">
+          Click me!
+        </div>
+      );
     }
-  }
+}
 
 export default App;
